03713ccd45927505dd690d7019217e64de2cc207
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-11-23 18:52:08 +0900

    html内にcss, js埋め込む形でアップロードファイル形式のPDF強制化を簡易実装。MDNサンプル参照：https://developer.mozilla.org/ja/docs/Web/HTML/Element/Input/file

--- filehandler.js
+++ filehandler.js
@@ -1,58 +1,72 @@
-const input = document.querySelector('#documents');
-const preview = document.querySelector('.preview');
+const input = document.querySelector('input[name="image_uploads"]');
+    const preview = document.querySelector('.preview');
+    console.log(input);
+    console.log(preview);
 
-input.style.opacity = 0;
+    input.style.opacity = 0;
 
-input.addEventListener('change', updateImageDisplay);
+    input.addEventListener('change', updateImageDisplay);
 
-function updateImageDisplay() {
-    while(preview.firstChild) {
-      preview.removeChild(preview.firstChild);
-    }
-  
-    const curFiles = input.files;
-    if (curFiles.length === 0) {
-      const para = document.createElement('p');
-      para.textContent = 'アップロードするファイルが選択されていません';
-      preview.appendChild(para);
-    } else {
-      const list = document.createElement('ol');
-      preview.appendChild(list);
-  
-      for (const file of curFiles) {
-        const listItem = document.createElement('li');
+    function updateImageDisplay() {
+      while(preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+      }
+
+      const curFiles = input.files;
+      if(curFiles.length === 0) {
         const para = document.createElement('p');
-        if (validFileType(file)) {
-          para.textContent = `ファイル名: ${file.name}, ファイルの長さ: ${returnFileSize(file.size)}.`;
-          const image = document.createElement('img');
-          image.src = URL.createObjectURL(file);
-  
-          listItem.appendChild(image);
-          listItem.appendChild(para);
-        } else {
-          para.textContent = `ファイル名: ${file.name}: ファイル形式が有効ではありません。選択しなおしてください。`;
-          listItem.appendChild(para);
+        para.textContent = 'No files currently selected for upload';
+        preview.appendChild(para);
+        
+      } else {
+        const list = document.createElement('ol');
+        preview.appendChild(list);
+
+        for(const file of curFiles) {
+          const listItem = document.createElement('li');
+          const para = document.createElement('p');
+
+          if(validFileType(file)) {
+            para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+            const image = document.createElement('img');
+            image.src = URL.createObjectURL(file);
+
+            listItem.appendChild(image);
+            listItem.appendChild(para);
+          } else {
+            para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+            listItem.appendChild(para);
+          }
+
+          list.appendChild(listItem);
         }
-  
-        list.appendChild(listItem);
       }
     }
-  }
 
-  const fileTypes = [
-    "application/pdf"
-  ];
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+    const fileTypes = [
+        'image/apng',
+        'image/bmp',
+        'image/gif',
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png',
+        'image/svg+xml',
+        'image/tiff',
+        'image/webp',
+        `image/x-icon`
+    ];
 
-  function validFileType(file) {
-    return fileTypes.includes(file.type);
-  }
-
-  function returnFileSize(number) {
-    if (number < 1024) {
-      return `${number} バイト`;
-    } else if (number >= 1024 && number < 1048576) {
-      return `${(number / 1024).toFixed(1)} KB`;
-    } else if (number >= 1048576) {
-      return `${(number / 1048576).toFixed(1)} MB`;
+    function validFileType(file) {
+      return fileTypes.includes(file.type);
     }
-  }
+
+    function returnFileSize(number) {
+      if(number < 1024) {
+        return number + 'bytes';
+      } else if(number > 1024 && number < 1048576) {
+        return (number/1024).toFixed(1) + 'KB';
+      } else if(number > 1048576) {
+        return (number/1048576).toFixed(1) + 'MB';
+      }
+    }
